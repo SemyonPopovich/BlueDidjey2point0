bc09801f4a78ec41b3c6ab8caa5d21a5372c7565
Semyon Popovich <popovichsema@yandex.ru>
2025-07-15 16:07:03 +0300

    Finished

--- BlueDidjey2point0/obj/Debug/net8.0/BlueDidjey2point0.AssemblyInfo.cs
+++ BlueDidjey2point0/obj/Debug/net8.0/BlueDidjey2point0.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlueDidjey2point0")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1761b617f85f3bca06b058b5e31e06e5eed95809")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlueDidjey2point0")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlueDidjey2point0")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
